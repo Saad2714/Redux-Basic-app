6198d0014dfa364a4ef5ebd44190e3bf4724b346
Saad Patel <patelsaadn@gmail.com>
2019-07-24 23:18:39 +0530

    commit2

--- App.js
+++ App.js
@@ -2,24 +2,47 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import { createStore } from "redux"
 import { Provider} from "react-redux"
+import MyApp from "./src/MyApp.js"
+
+
 const initializeState = {
   myCounter: 0,
 
 }
 // No need to use redux.createStore as we already imported
 
-const store = createStore();
+const reducer = ( state = initializeState, action) => {
+
+  switch (action.type) {
+    case "INC_COUNTER":
+      return {myCounter: state.myCounter + 1 }
+
+    case "DEC_COUNTER":
+    if (state.myCounter >= 1) {
+     return {myCounter: state.myCounter - 1 }
+       
+    } else { 
+      return state
+    }   
+    
+  }
+
+  return state
+}
+
+const store = createStore(reducer);
 
 
 export default class App extends React.Component{
 render(){
   return (
-    <View style={styles.container}>
-      <Text>Let's create An App</Text>
-    </View>
+       <Provider store = {store}>
+       <MyApp />  
+       </Provider> 
   );
 }
 }
+
 
 
 const styles = StyleSheet.create({
